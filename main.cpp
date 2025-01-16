d850244d6541d0a8959f31df0658379ffec05675
r.jumanazarova <r.jumanazarova@newuu.uz>
2025-01-16 14:43:00 +0500

    Name asking

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!" << endl;
+    int name;
+    cout << "Enter your name?" << endl;
+   cin>>name;
+   cout<<"Your name is "<<name<<endl;
+    return 0;
+}
